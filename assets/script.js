c0124bfaaf1d6ab436a1687a0b40aac6a563809a
Giovane <giovane.cardoso@corebiz.com.br>
2019-10-14 09:11:49 -0300

    Alteracao do nome do projeto e alteracao de id para class

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,41 @@
 $(document).ready(function(){
+  $(".contentbox1").hide();
 $("#dropdown").click(function(){
-    $("#menudesk").toggle();
+    $(".menudesk").toggle();
     
   });
   if(window.screen.width > 360){
   $("#plus1").click(function(){
-    $("#contentbox1").toggle("fast");
+    $(".contentbox1").toggle("fast");
     
   });
   $("#plus2").click(function(){
 
-    $("#contentbox2").toggle("fast");
+    $(".contentbox2").toggle("fast");
     
   });
   $("#plus3").click(function(){
 
-    $("#contentbox3").toggle("fast");
+    $(".contentbox3").toggle("fast");
     
   });
 }else{
   $("#plus1").click(function(){
-    $("#contentbox1").toggle("fast");
+    $(".contentbox1").toggle("fast");
     
   });
   $("#plus2").click(function(){
-    $("#contentbox2").toggle("fast");
+    $(".contentbox2").toggle("fast");
     
   });
   $("#plus3").click(function(){
-    $("#contentbox3").toggle("fast");
+    $(".contentbox3").toggle("fast");
     
   });
+var testevalida = $(".procurar").length();
+if (testevalida > 0){
+  $(".search").hide();
+}
+
 }
 });
