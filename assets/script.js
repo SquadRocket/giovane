af757ecdaa7e5fafcc0479578d90269f63d4770a
Giovane <giovane.cardoso@corebiz.com.br>
2019-10-14 11:28:33 -0300

    Animacao leia mais/menos

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
   $(".contentbox1").hide();
+  $("#teste").hide();
+  $("#mostrarmenos").hide();
+
+
 $("#dropdown").click(function(){
     $(".menudesk").toggle();
     
   });
-  if(window.screen.width > 360){
+
   $("#plus1").click(function(){
     $(".contentbox1").toggle("fast");
     
@@ -19,23 +23,21 @@ $("#dropdown").click(function(){
     $(".contentbox3").toggle("fast");
     
   });
-}else{
-  $("#plus1").click(function(){
-    $(".contentbox1").toggle("fast");
-    
-  });
-  $("#plus2").click(function(){
-    $(".contentbox2").toggle("fast");
-    
-  });
-  $("#plus3").click(function(){
-    $(".contentbox3").toggle("fast");
-    
-  });
-var testevalida = $(".procurar").length();
-if (testevalida > 0){
-  $(".search").hide();
-}
+  $("#mostrarmais").click(function(){
+    $("#teste").show();
+    $("#mostrarmenos").show();
+    $("#mostrarmais").hide();
 
-}
+
+
+  });
+  $("#mostrarmenos").click(function(){
+    $("#teste").hide();
+    $("#mostrarmenos").hide();
+    $("#mostrarmais").show();
+
+
+
+  });
 });
+
