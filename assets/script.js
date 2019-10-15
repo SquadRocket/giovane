57b5aa11b0b02dd3fa0ffadd24b5cf9ebe3f567c
Giovane <giovane.cardoso@corebiz.com.br>
2019-10-15 15:07:48 -0300

    transformacao de jquery para js puro

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,85 @@
+
+function slidetoggle(){
+  var trocar = document.getElementById("menudesk");
+  if (trocar.style.display === "none"){
+    trocar.style.display = "block";
+  }else{
+    trocar.style.display = "none";
+  }
+
+}
+
+
+
+function togglebox1(){
+  var trocar = document.getElementById("contentbox1");
+  if (trocar.style.display === "none"){
+    trocar.style.display = "flex";
+  }else{
+    trocar.style.display = "none";
+  }
+}
+
+function togglebox2(){
+  var trocar = document.getElementById("contentbox2");
+    if (trocar.style.display === "none"){
+    trocar.style.display = "flex";
+  }else{
+    trocar.style.display = "none";
+  }
+}
+
+function togglebox3(){
+  var trocar = document.getElementById("contentbox3");
+  if (trocar.style.display === "none"){
+    trocar.style.display = "flex";
+  }else{
+    trocar.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
   $(".contentbox1").hide();
+  $(".contentbox2").hide();
+  $(".contentbox3").hide();
   $("#teste").hide();
-  $("#mostrarmenos").hide();
 
-
-$("#dropdown").click(function(){
-    $(".menudesk").toggle();
-    
-  });
-
-  $("#plus1").click(function(){
-    $(".contentbox1").toggle("fast");
-    
-  });
-  $("#plus2").click(function(){
-
-    $(".contentbox2").toggle("fast");
-    
-  });
-  $("#plus3").click(function(){
-
-    $(".contentbox3").toggle("fast");
-    
-  });
   $("#mostrarmais").click(function(){
     $("#teste").show();
     $("#mostrarmenos").show();
     $("#mostrarmais").hide();
-
-
-
   });
   $("#mostrarmenos").click(function(){
     $("#teste").hide();
     $("#mostrarmenos").hide();
     $("#mostrarmais").show();
-
-
-
   });
 });
-
