6f135302f6c66605c4bfdaa3b5f81c377c365d33
Giovane <giovane.cardoso@corebiz.com.br>
2019-10-23 16:55:47 -0300

    fix box-shadow

--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,37 @@
-function slidetoggle(){
-  var trocar = document.getElementById("menudesk");
-  if (trocar.style.display === "none"){
-    trocar.style.display = "block";
-  }else{
-    trocar.style.display = "none";
-  }
-
-}
-
-
-
-function togglebox1(){
-  var trocar = document.getElementById("contentbox1");
-  if (trocar.style.display === "none"){
-    trocar.style.display = "flex";
-  }else{
-    trocar.style.display = "none";
-  }
-}
-
-function togglebox2(){
-  var trocar = document.getElementById("contentbox2");
-    if (trocar.style.display === "none"){
-    trocar.style.display = "flex";
-  }else{
-    trocar.style.display = "none";
-  }
-}
-
-function togglebox3(){
-  var trocar = document.getElementById("contentbox3");
-  if (trocar.style.display === "none"){
-    trocar.style.display = "flex";
-  }else{
-    trocar.style.display = "none";
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 $(document).ready(function(){
-  // $(".contentbox1").hide();
-  // $(".contentbox2").hide();
-  // $(".contentbox3").hide();
+  $(".contentbox1").hide();
+  $(".contentbox2").hide();
+  $(".contentbox3").hide();
   $("#teste").hide();
+  $(".navp").click(function(){
+    $(".menudesk").slideToggle("fast");
+  
 
+
+  });
   $("#mostrarmais").click(function(){
-    $("#teste").show();
+    $("#teste").show(500);
     $("#mostrarmenos").show();
     $("#mostrarmais").hide();
   });
   $("#mostrarmenos").click(function(){
-    $("#teste").hide();
+    $("#teste").hide(500);
     $("#mostrarmenos").hide();
     $("#mostrarmais").show();
   });
+  $(".box1").click(function(){
+    $(".contentbox1").slideToggle(500);
+
+  });
+  $(".box2").click(function(){
+    $(".contentbox2").slideToggle(700);
+
+  });
+  $(".box3").click(function(){
+    $(".contentbox3").slideToggle(500);
+
+  });
+
+  
 });
